48580e3311f202812b242246c808df13af1e1495
John Mitchell <jrmmba@outlook.com>
2022-03-15 16:38:35 -0700

    adding redux

---
+++ react-redux-with-routing/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello React Lovers!
+    </div>
+  );
+}
+
+export default App;
